9f9f3f9aadc873ddc979cd23a0cf8d1439224ab3
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-09-29 12:12:45 +0200

    creazione base

---
+++ src/main/java/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(long id) {
+        super("Il record con id " + id + " non è stato trovato!");
+    }
+}
