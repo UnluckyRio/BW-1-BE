39c057405fda743d8fe6a1d5691201f364b118e1
GerardoCasciano <dinocasciano203@gmail.com>
2025-09-30 09:26:42 +0200

    gery

--- src/main/java/entities/Tratta.java
+++ src/main/java/entities/Tratta.java
@@ -1,4 +1,52 @@
 package entities;
 
+import java.time.LocalDate;
+
 public class Tratta {
+    private Long id;
+    private LocalDate dataOraInizio;
+    private int tempo_effettivo;
+
+    public Tratta() {
+    }
+
+
+    public Tratta(Long id, LocalDate dataOraInizio, int tempo_effettivo) {
+        this.id = id;
+        this.dataOraInizio = dataOraInizio;
+        this.tempo_effettivo = tempo_effettivo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataOraInizio() {
+        return dataOraInizio;
+    }
+
+    public void setDataOraInizio(LocalDate dataOraInizio) {
+        this.dataOraInizio = dataOraInizio;
+    }
+
+    public int getTempo_effettivo() {
+        return tempo_effettivo;
+    }
+
+    public void setTempo_effettivo(int tempo_effettivo) {
+        this.tempo_effettivo = tempo_effettivo;
+    }
+
+    @Override
+    public String toString() {
+        return "Tratta{" +
+                "id=" + id +
+                ", dataOraInizio=" + dataOraInizio +
+                ", tempo_effettivo=" + tempo_effettivo +
+                '}';
+    }
 }
