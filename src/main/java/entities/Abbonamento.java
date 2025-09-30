8e7c4fb5135bdd92ff2bdd388ed8c7e3fd89e84c
AminTabite <Aminepicode13@gmail.com>
2025-09-30 17:37:22 +0200

    start
    ...

--- src/main/java/entities/Abbonamento.java
+++ src/main/java/entities/Abbonamento.java
@@ -1,4 +1,99 @@
 package entities;
 
+import enums.TipoAbbonamento;
+import enums.TipoTitoloViaggio;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Abbonamento")
+
+
 public class Abbonamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "abbonamento_id", sequenceName = "abbonamento_id", allocationSize = 1)
+    protected long id;
+
+
+    @Column(name = "Data Emissione")
+    protected LocalDate dataEmissione;
+
+    @Column(name = "prezzo")
+    protected double prezzo;
+
+
+    // @Column(name = "id_rivenditore", nullable = false)
+    //  protected Rivenditore idrivenditore;
+
+
+    // @Column(name = "id_distributore", nullable = false)
+    //  protected Rivenditore iddistributore;
+
+
+    @Column(name = "Tipo abbonamento", nullable = false)
+    private TipoAbbonamento tipoAbbonamento;
+
+    @Column(name = "Data inizio iscrizione", nullable = false)
+    private LocalDate dataiscrizione;
+
+    @Column(name = "Data scadenza iscrizione", nullable = false)
+    private LocalDate datascadenza;
+
+    @OneToOne
+    @JoinColumn(name = "id_validazione")
+    private Validazione validazione;
+
+    public Abbonamento(TipoTitoloViaggio tipoTitoloViaggio, LocalDate dataEmissione, double prezzo, TipoAbbonamento tipoAbbonamento, LocalDate dataiscrizione, LocalDate datascadenza, Validazione validazione) {
+
+        this.id = id;
+        this.tipoAbbonamento = tipoAbbonamento;
+        this.dataiscrizione = dataiscrizione;
+        this.datascadenza = datascadenza;
+        this.validazione = validazione;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public TipoAbbonamento getTipoAbbonamento() {
+        return tipoAbbonamento;
+    }
+
+    public void setTipoAbbonamento(TipoAbbonamento tipoAbbonamento) {
+        this.tipoAbbonamento = tipoAbbonamento;
+    }
+
+    public LocalDate getDataiscrizione() {
+        return dataiscrizione;
+    }
+
+    public void setDataiscrizione(LocalDate dataiscrizione) {
+        this.dataiscrizione = dataiscrizione;
+    }
+
+    public LocalDate getDatascadenza() {
+        return datascadenza;
+    }
+
+    public void setDatascadenza(LocalDate datascadenza) {
+        this.datascadenza = datascadenza;
+    }
+
+    public Validazione getValidazione() {
+        return validazione;
+    }
+
+    public void setValidazione(Validazione validazione) {
+        this.validazione = validazione;
+    }
+
 }
--- src/main/java/entities/Biglietto.java
+++ src/main/java/entities/Biglietto.java
@@ -1,4 +1,88 @@
 package entities;
 
+import enums.TipoTitoloViaggio;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Biglietto")
+
+
 public class Biglietto {
+
+    @Column(name = "Data Emissione")
+    protected LocalDate dataEmissione;
+    @Column(name = "prezzo")
+    protected double prezzo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "biglietto_id", sequenceName = "biglietto_id", allocationSize = 1)
+    private long id;
+
+
+    // @Column(name = "id_rivenditore", nullable = false)
+    //  protected Rivenditore idrivenditore;
+
+
+    // @Column(name = "id_distributore", nullable = false)
+    //  protected Rivenditore iddistributore;
+    @Column(name = "Timbrato", nullable = false)
+    private boolean timbrato;
+
+    @Column(name = "durata validazione", nullable = false)
+    private int durataValidazione;
+
+    @OneToOne
+    @JoinColumn(name = "id_validazione")
+    private Validazione validazione;
+
+
+    public Biglietto() {
+
+    }
+
+
+    public Biglietto(TipoTitoloViaggio tipoTitoloViaggio, LocalDate dataEmissione, double prezzo, boolean timbrato, int durataValidazione, Validazione validazione) {
+
+        this.id = id;
+        this.timbrato = timbrato;
+        this.durataValidazione = durataValidazione;
+        this.validazione = validazione;
+    }
+
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isTimbrato() {
+        return timbrato;
+    }
+
+    public void setTimbrato(boolean timbrato) {
+        this.timbrato = timbrato;
+    }
+
+    public int getDurataValidazione() {
+        return durataValidazione;
+    }
+
+    public void setDurataValidazione(int durataValidazione) {
+        this.durataValidazione = durataValidazione;
+    }
+
+    public Validazione getValidazione() {
+        return validazione;
+    }
+
+    public void setValidazione(Validazione validazione) {
+        this.validazione = validazione;
+    }
+
+
 }
