710ffc4bc66142a578eddb3ea9e1f81ea936a42b
Dade1991 <dade91@msn.com>
2025-10-01 17:22:15 +0200

    DAOs for: Manutenzione, Tratta, PercorrenzaMedia

--- src/main/java/dao/PercorrenzaMediaDAO.java
+++ src/main/java/dao/PercorrenzaMediaDAO.java
@@ -1,4 +1,33 @@
 package dao;
 
+import entities.PercorrenzaMedia;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class PercorrenzaMediaDAO {
+    private final EntityManager em;
+
+    public PercorrenzaMediaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    // Salvataggio
+
+    public void save (PercorrenzaMedia newPercorrenzaMedia) {
+        try {
+            EntityTransaction transaction = em.getTransaction();
+            transaction.begin();
+            em.persist(newPercorrenzaMedia);
+            transaction.commit();
+            System.out.println("La percorrenza media");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    // Ricerca
+
+    public PercorrenzaMedia findAvaragePathById (long id) {
+        return em.find(PercorrenzaMedia.class, id);
+    }
 }
