0c616011470cc8bde458937e5d7c8f80f765f7db
AminTabite <Aminepicode13@gmail.com>
2025-10-01 17:04:09 +0200

    Dao base biglietto/abbonamento
    ok

--- src/main/java/dao/AbbonamentoDAO.java
+++ src/main/java/dao/AbbonamentoDAO.java
@@ -1,4 +1,58 @@
 package dao;
 
+import entities.Abbonamento;
+import entities.Biglietto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class AbbonamentoDAO {
+
+
+    private final EntityManager entityManager;
+
+
+    public AbbonamentoDAO(EntityManager entityManager, EntityManager entityManager1){
+
+        this.entityManager = entityManager;
+    }
+
+
+    public void save(Abbonamento newAbbonamento) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try{
+            transaction.begin();
+
+            entityManager.persist(newAbbonamento);
+
+            transaction.commit();
+
+            System.out.println("Biglietto salvato correttamente" + newAbbonamento);
+
+
+        } catch (Exception e) {if(transaction.isActive()){
+            transaction.rollback();
+        }
+            System.out.println("Errore salvataggio biglietto");
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public Abbonamento findById(long Abbonamentoid){
+        try {
+
+
+            Abbonamento trovato = entityManager.find(Abbonamento.class, Abbonamentoid);
+
+            return trovato;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
--- src/main/java/dao/BigliettoDAO.java
+++ src/main/java/dao/BigliettoDAO.java
@@ -1,4 +1,59 @@
 package dao;
 
+import entities.Biglietto;
+import exceptions.InvalidDataException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.transaction.Transaction;
+
 public class BigliettoDAO {
+
+
+    private final EntityManager entityManager;
+
+
+    public BigliettoDAO(EntityManager entityManager, EntityManager entityManager1){
+
+        this.entityManager = entityManager;
+    }
+
+
+    public void save(Biglietto newBiglietto) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try{
+            transaction.begin();
+
+            entityManager.persist(newBiglietto);
+
+        transaction.commit();
+
+        System.out.println("Biglietto salvato correttamente" + newBiglietto);
+
+
+        } catch (Exception e) {if(transaction.isActive()){
+            transaction.rollback();
+        }
+            System.out.println("Errore salvataggio biglietto");
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public Biglietto findById(long bigliettoid){
+     try {
+
+
+         Biglietto trovato = entityManager.find(Biglietto.class, bigliettoid);
+
+         return trovato;
+     } catch (Exception e) {
+         throw new RuntimeException(e);
+     }
+    }
+
+
+
+
 }
