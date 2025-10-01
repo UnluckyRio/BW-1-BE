a698a61f3987abe5b26933c139e632d2f7c803ed
GerardoCasciano <dinocasciano203@gmail.com>
2025-10-01 17:22:59 +0200

    gery

--- src/main/java/dao/DistributoreAutomaticoDAO.java
+++ src/main/java/dao/DistributoreAutomaticoDAO.java
@@ -1,4 +1,31 @@
 package dao;
 
+import entities.DistributoreAutomatico;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+
 public class DistributoreAutomaticoDAO {
+    private final EntityManager em;
+
+
+    public DistributoreAutomaticoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(DistributoreAutomatico newdistributoreAutomatico){
+        EntityTransaction transaction=em.getTransaction();
+        try{
+            transaction.begin();
+            em.persist(newdistributoreAutomatico);
+            transaction.commit();
+            System.out.println("Erogazione" + newdistributoreAutomatico + "avvenuta con successo");
+        }catch (Exception e){
+            if(transaction.isActive()){
+                transaction.rollback();
+            }
+            System.out.println("Errore erogazione");
+            e.printStackTrace();
+        }
+
+    }
 }
