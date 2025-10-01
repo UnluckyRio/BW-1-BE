c817f12c2996fb28c9689b0173276b63150deac2
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-01 17:35:13 +0200

    Utente e Tessera Dao creati

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,9 +3,13 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Application {
+
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw1be");
+
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
 
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -1,4 +1,59 @@
 package dao;
 
+import entities.Utente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+
 public class UtenteDAO {
+
+    private final EntityManager em;
+
+    public UtenteDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void create(Utente utente) {
+        EntityTransaction transition = em.getTransaction();
+        try {
+            transition.begin();
+            em.persist(utente);
+            transition.commit();
+        } catch (Exception e) {
+            if (transition.isActive()) transition.rollback();
+            throw e;
+        }
+    }
+
+    public Utente findById(long id) {
+        return em.find(Utente.class, id);
+    }
+
+    public List<Utente> findAll() {
+        return em.createQuery("SELECT u FROM Utente u", Utente.class).getResultList();
+    }
+
+    public void update(Utente utente) {
+        EntityTransaction transition = em.getTransaction();
+        try {
+            transition.begin();
+            em.merge(utente);
+            transition.commit();
+        } catch (Exception e) {
+            if (transition.isActive()) transition.rollback();
+            throw e;
+        }
+    }
+
+    public void delete(Utente utente) {
+        EntityTransaction transition = em.getTransaction();
+        try {
+            transition.begin();
+            em.remove(em.contains(utente) ? utente : em.merge(utente));
+            transition.commit();
+        } catch (Exception e) {
+            if (transition.isActive()) transition.rollback();
+            throw e;
+        }
+    }
 }
